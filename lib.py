b12b09ff0375bd59713b3ce6c0f0fad962ce1110
TianoRao <rtj0808@outlook.com>
2023-10-26 22:20:03 -0400

    Update files

--- lib.py
+++ lib.py
@@ -7,13 +7,16 @@ import matplotlib.pyplot as plt
 def pd_desc(df):
     return df.describe()
 
+
 # mean
 def mean(df):
     return df.mean()
 
+
 # median
 def median(df):
     return df.median()
+
 
 # std
 def std(df):
@@ -21,16 +24,17 @@ def std(df):
 
 
 # # visualization: bar plot
-def pd_visual(df, render=True): 
+def pd_visual(df, render=True):
     df.plot(kind="hist")
-    plt.xticks(rotation = 90)
-    plt.ylabel('Frequency')
+    plt.xticks(rotation=90)
+    plt.ylabel("Frequency")
     plt.xlabel(df.name)
     if render:
         plt.show()
     else:
-        plt.savefig('histgram.png')
-    
+        plt.savefig("histgram.png")
+
+
 # # For Polars
 # # describe
 # def polars_desc(df):
--- test_main.py
+++ test_main.py
@@ -1,9 +1,11 @@
 from main import summary_desc, visual, save_to_md
+
 # import pandas as pd
+
 
 def test_summary_desc():
     result = summary_desc()
-    
+
     # mean
     assert result.loc["mean", "Electric Range"] == 70.49573804284242
 
@@ -13,12 +15,15 @@ def test_summary_desc():
     # standard deviation
     assert round(result.loc["std", "Electric Range"], 6) == 97.128735
 
+
 def test_visual():
     visual()
-    
+
+
 def test_save_to_md():
     save_to_md()
-    
+
+
 if __name__ == "__main__":
     test_summary_desc()
     test_visual()
